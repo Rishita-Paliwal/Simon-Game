ca7d04cf542596ebc733d5c02d98aa52c04a8416
Rishita-Paliwal <rishitapaliwal.atwork@gmail.com>
2025-07-31 14:17:30 +0530

    Initial commit: Simon Game project

---
+++ game.js
@@ -0,0 +1,87 @@
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+// Start game on any keypress or click on title (for mobile)
+$(document).on("keypress click", function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    playSound("wrong");
+
+    // Add game over animation
+    $("body").addClass("game-over");
+    $("#level-title").text("💥 Game Over! Tap or Press Any Key to Restart 💥");
+
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 300);
+
+    startOver();
+  }
+}
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  // Fancy button flash animation
+  $("#" + randomChosenColour)
+    .fadeOut(100)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  playSound(randomChosenColour);
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play().catch(() => {
+    console.log("Audio blocked by browser. Interaction required.");
+  });
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
